a3c2578d3d6a52d42b8c25aa0fc7f753bc2ddd14
xxsawa <xsawa@stuba.sk>
2024-10-22 22:42:16 +0200

    Starting db schema created and synced with db

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,40 @@
-export default function Home() {
-    return (
-        <div className="flex h-screen w-screen items-center justify-center">
-            Hello world!
-        </div>
-    );
+import { PrismaClient } from "@prisma/client";
+
+
+var db: PrismaClient = new PrismaClient()
+
+
+export default async function Home() {
+    var allUsers;
+    try {
+        allUsers = await db.users.findMany({
+            where: {
+                name: "Daniel"
+            }
+        });
+
+        if (allUsers.length == 0) {
+            await db.users.create({
+                data: {
+                    name: "Daniel",
+                    email: "daniel@sawa.sk"
+                }
+            })
+        }
+        console.log(allUsers)
+        return (
+            <div className="flex h-screen w-screen items-center justify-center">
+                {allUsers.length ? allUsers[0].name : "no users, refresh this page!"}
+            </div>
+        );
+    }
+    catch {
+        return (
+            <div className="flex h-screen w-screen items-center justify-center">
+                No db conncetion!
+            </div>
+        );
+    }
+
+
 }
