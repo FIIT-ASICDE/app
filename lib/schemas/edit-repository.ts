08381e99340bc31885444c1e77e09940e84d6b9d
Lukas Castven <xcastven@stuba.sk>
2025-02-24 21:39:36 +0100

    Add limits to repo schemas

--- lib/schemas/edit-repository.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-
-export const editRepositoryFormSchema = z.object({
-    name: z.string().min(1, "Repository name is required."),
-    description: z.string().optional(),
-});
