2ff4157790abed41e42ddd7d81bbb0759b23f691
Maximilián Strečanský <xstrecansky@gmail.com>
2025-03-13 13:04:42 +0000

    Org repo skeleton

---
+++ app/[userslug]/(profile)/repositories/loading.tsx
@@ -0,0 +1,22 @@
+import { Skeleton } from "@/components/ui/skeleton"
+import { RepoOrgSubmenuSkeleton } from "@/components/skeletons/repo-org-submenu-skeleton";
+import { FolderPlus } from "lucide-react";
+import { RepoOrgCardSkeleton } from "@/components/skeletons/repo-org-card-skeleton";
+
+export default function Loading() {
+    return (
+        <Skeleton className="bg-background text-foreground">
+            <RepoOrgSubmenuSkeleton
+                searchText="Search repositories..."
+                createText="Create repository"
+                icon={FolderPlus}
+            />
+            <main>
+                <div className="m-6 grid grid-cols-1 gap-3 lg:grid-cols-2">
+                    <RepoOrgCardSkeleton/>
+                    <RepoOrgCardSkeleton/>
+                </div>
+            </main>
+        </Skeleton>
+    );
+}
