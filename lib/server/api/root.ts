70e3d7fc195aed1f0aabd1ca72a25c8a843cb8f9
Adam Grik <adam4grik@gmail.com>
2025-04-06 12:20:52 +0000

    Simulation first working version

--- lib/server/api/root.ts
+++ lib/server/api/root.ts
@@ -4,6 +4,7 @@ import { orgRouter } from "@/lib/server/api/routers/orgs";
 import { repoRouter } from "@/lib/server/api/routers/repos";
 import { userRouter } from "@/lib/server/api/routers/user";
 import { createCallerFactory, createTRPCRouter } from "@/lib/server/api/trpc";
+import { simulationRouter } from "@/lib/server/api/routers/simulation";
 
 /**
  * This is the primary router for your server.
@@ -16,6 +17,7 @@ export const appRouter = createTRPCRouter({
     repo: repoRouter,
     git: gitRouter,
     editor: editorRouter,
+    simulation: simulationRouter
 });
 
 // export type definition of API
