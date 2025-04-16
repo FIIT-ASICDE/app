ed9da50e75916671c5b2f7e5a266b52a578b71c1
Lukas Castven <xcastven@stuba.sk>
2025-04-16 08:34:45 +0200

    Update lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,11 +10,14 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
     {
         rules: {
             "no-restricted-imports": ["error", { patterns: ["../*", "./*"] }],
         },
+    },
+    {
+        ignores: ["node_modules", ".next/", "lib/prisma/**"],
     },
 ];
 
