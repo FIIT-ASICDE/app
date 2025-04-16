04218e62618b28eeb5245278d20b41333bd984e7
Lukas Castven <xcastven@stuba.sk>
2025-04-16 15:04:29 +0200

    Test whole deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ bun install --production
 bunx prisma generate
 
 if [ -z "$(pm2 pid asicde)" ]; then
-	pm2 start npm --name "asicde" -i 2 -- start
+	pm2 start bun --name "asicde" --time -- start
 	pm2 save
 else
 	pm2 reload asicde --update-env
