5bace575d54ea725eeff69e6c439054569b6e839
Lukas Castven <xcastven@stuba.sk>
2025-04-16 08:37:11 +0200

    Update lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,12 +5,10 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-});
+const compat = new FlatCompat({ baseDirectory: __dirname });
 
 const eslintConfig = [
-    ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+    ...compat.extends("next", "next/typescript", "prettier"),
     {
         rules: {
             "no-restricted-imports": ["error", { patterns: ["../*", "./*"] }],
